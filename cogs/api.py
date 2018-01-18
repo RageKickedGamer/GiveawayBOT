066fd886aea85a19d52b3e098bb41bb61375f2ec
Sam <34219071+RageKickedGamer@users.noreply.github.com>
2018-01-17 21:07:41 -0800

    Update api.py

--- cogs/api.py
+++ cogs/api.py
@@ -13,20 +13,20 @@ headers = {"Authorization" : dbltoken}
 
 class api():
 
-async def on_ready():
-    payload = {"server_count"  : len(bot.servers)}
-    async with aiohttp.ClientSession() as aioclient:
-            await aioclient.post(url, data=payload, headers=headers)
+    async def on_ready():
+        payload = {"server_count"  : len(bot.servers)}
+        async with aiohttp.ClientSession() as aioclient:
+                await aioclient.post(url, data=payload, headers=headers)
 
-async def on_server_join(server):
-    payload = {"server_count"  : len(bot.servers)}
-    async with aiohttp.ClientSession() as aioclient:
-            await aioclient.post(url, data=payload, headers=headers)
+    async def on_server_join(server):
+        payload = {"server_count"  : len(bot.servers)}
+        async with aiohttp.ClientSession() as aioclient:
+                await aioclient.post(url, data=payload, headers=headers)
 
-async def on_server_remove(server):
-    payload = {"server_count"  : len(bot.servers)}
-    async with aiohttp.ClientSession() as aioclient:
-            await aioclient.post(url, data=payload, headers=headers)
+    async def on_server_remove(server):
+        payload = {"server_count"  : len(bot.servers)}
+        async with aiohttp.ClientSession() as aioclient:
+                await aioclient.post(url, data=payload, headers=headers)
       
 def setup(bot):
     bot.add_cog(api)
