f95541a7d3de7063e4e47b3188b3888b592cfa7a
Sam <34219071+RageKickedGamer@users.noreply.github.com>
2018-01-17 20:40:16 -0800

    Update api.py

--- cogs/api.py
+++ cogs/api.py
@@ -3,7 +3,7 @@ import os
 import requests
 import discord
 
-from discrd.ext import commands
+from discord.ext import commands
 from discord.ext.commands import Bot
 
 bot = commands.Bot(command_prefix = commands.when_mentioned_or("+g"))
