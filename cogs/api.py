4f4061c45c0c19b13ce686230793fd9e4cb39ea4
Sam <34219071+RageKickedGamer@users.noreply.github.com>
2018-01-17 20:35:28 -0800

    Update api.py

--- cogs/api.py
+++ cogs/api.py
@@ -1,32 +1,24 @@
-import discord
 import aiohttp
-import asyncio
 import os
 import requests
 
-from discord.ext import commands
-
-bot = commands.Bot(command_prefix = commands.when_mentioned_or ("+g"))
-BOT_ID = "396464677032427530"
-AUTH = os.environ.get('DBLT')
-token = AUTH
-url = "https://discordbots.org/api/bots/{}/stats".format(BOT_ID)
-headers = {"Authorization" : token}
-
 class api():
     
+    dbltoken = os.environ.get('DBLT')
+    url = "https://discordbots.org/api/bots/" + bot.user.id + "/stats"
+    headers = {"Authorization" : dbltoken}
+
     async def on_ready():
         payload = {"server_count"  : len(bot.servers)}
         requests.post(url, data=payload, headers=headers)
-    
+
     async def on_server_join(server):
         payload = {"server_count"  : len(bot.servers)}
         requests.post(url, data=payload, headers=headers)
-    
+
     async def on_server_remove(server):
         payload = {"server_count"  : len(bot.servers)}
         requests.post(url, data=payload, headers=headers)
       
-
 def setup(bot):
     bot.add_cog(api)
