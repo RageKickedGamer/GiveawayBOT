8e6181ad7876ce1773250b6da2fcfc2afa3df7ef
Sam <34219071+RageKickedGamer@users.noreply.github.com>
2018-01-17 21:12:52 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ from discord.ext.commands import Bot
 from random import randint
 
 bot = commands.Bot(command_prefix = commands.when_mentioned_or("+g"))
-startup_extensions = ["cogs.create", "cogs.start", "cogs.api"]
+startup_extensions = ["cogs.create", "cogs.start"]
 logs = discord.Object("403024766056792074")
 
 for extension in startup_extensions:
