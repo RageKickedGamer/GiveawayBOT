ca0ca3ab0f0db2bf98e509855e184e5abc1dffae
Sam <34219071+RageKickedGamer@users.noreply.github.com>
2018-01-17 19:02:11 -0800

    Add files via upload

---
+++ cogs/api.py
@@ -0,0 +1,33 @@
+import discord
+import aiohttp
+import asyncio
+import os
+import requests
+
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix = commands.when_mentioned_or ("+g"))
+BOT_ID = "396464677032427530"
+AUTH = os.environ.get('DBLT')
+url = "https://discordbots.org/api/bots/{}/stats".format(BOT_ID)
+headers = {"Authorization" : AUTH}
+
+class api():
+    @bot.event
+    async def on_ready():
+        payload = {"server_count"  : len(ctx.bot.servers)}
+        requests.post(url, data=payload, headers=headers)
+    
+    @bot.event
+    async def on_server_join(server):
+        payload = {"server_count"  : len(ctx.bot.servers)}
+        requests.post(url, data=payload, headers=headers)
+    
+    @bot.event
+    async def on_server_remove(server):
+        payload = {"server_count"  : len(ctx.bot.servers)}
+        requests.post(url, data=payload, headers=headers)
+      
+
+def setup(bot):
+    bot.add_cog(api)
